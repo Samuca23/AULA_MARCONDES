a9ac1e93571c8c790b8ef4ce7f04bb97090ff9f3
Samuel Chiodini <samuelchiodini@hotmail.com>
2021-10-06 20:16:55 -0300

    Finalizado

--- Aula_29_09/processo.php
+++ Aula_29_09/processo.php
@@ -1,10 +1,10 @@
 <?php
 
-const VALOR_FIXO = 1000;
+const VALOR_FIXO = 100;
 
-    $iLinha = isset($_POST['linha']) ? $_POST['linha'] : null;   
-    $iColuna = isset ($_POST['coluna']) ? $_POST['coluna'] : null;
-    $iQuntPorPagina = isset($_POST['qnt_por_pagina']) ? $_POST['qnt_por_pagina'] : null;
+    $iLinha = isset($_POST['linha']) ? $_POST['linha'] : $_GET['linha'];   
+    $iColuna = isset ($_POST['coluna']) ? $_POST['coluna'] : $_GET['coluna'];
+    $iQuntPorPagina = isset($_POST['qnt_por_pagina']) ? $_POST['qnt_por_pagina'] : $_GET['qnt_por_pagina'];
         
     $aVetor = [];
 var_dump($iLinha);
@@ -22,7 +22,6 @@ var_dump($iQuntPorPagina);
      for ($i = (($iQuntPorPagina * $iLinha) - $iLinha); $i < $iLinha * $iQuntPorPagina; $i++) {
         echo '<tr>';
         echo '<td>';
-        echo $i;
         echo '</td>';
         for($j = 0; $j < $iColuna; $j++) {
             echo '<td>';
@@ -34,9 +33,10 @@ var_dump($iQuntPorPagina);
  
           
     echo '</table>';
-
+var_dump((VALOR_FIXO/($iColuna * $iLinha)));
     //links
-    for ($i = 1; $i <= VALOR_FIXO/$iLinha; $i++) {
+    for ($i = 1; $i <= (VALOR_FIXO/($iColuna * $iLinha)); $i++) {
         echo "<a href='?qnt_por_pagina={$i}&coluna={$iColuna}&linha={$iLinha}'>{$i}</a> ";
     }
+
 ?>
